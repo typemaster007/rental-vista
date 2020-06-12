9bd9f58c13579b700afecb3c236a46018ed69fbb
Sahil Fruitwala <sahilfruitwala007@gmail.com>
2020-06-12 16:33:35 -0300

    Fixed Rendering issue in OtherPages.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -7,34 +7,65 @@ import OtherPages from "./otherPages/OtherPages";
 const initialMessage = {
   title: "",
   body: "",
-  show: false
 };
 
 function ProfileManagement() {
-  console.log("IN INDEX")
   const [message, setMessage] = useState(initialMessage);
 
   const handleOnClick = (msg) => {
     setMessage({
       ...msg,
-      show: message.title !== "" ? false : true
     });
-  }
+  };
 
   return (
     <div>
       <div className="sidebar">
-        <a href="" onClick={() => handleOnClick(initialMessage)}>Edit Profile</a>
-        <a href="" onClick={() => handleOnClick({ title: "Success!", body: "Your Password has been changed" })}>Reset Password</a>
-        <a href=""  onClick={() => handleOnClick({ title: "Sorry!", body: "Currently, you do not have any saved rooms!" })}>Saved Rooms</a>
-        <a href=""  onClick={() => handleOnClick({ title: "Sorry!", body: "Currently, you do not have any contact request approved!"})}>Requested Contacts</a>
+        <button href="" onClick={() => handleOnClick(initialMessage)}>
+          Edit Profile
+        </button>
+        <button
+          onClick={() =>
+            handleOnClick({
+              title: "Success!",
+              body: "Your Password has been changed",
+            })
+          }
+        >
+          Reset Password
+        </button>
+        <button
+          href=""
+          onClick={() =>
+            handleOnClick({
+              title: "No Saved Rooms!",
+              body: "Currently, you do not have any saved rooms!",
+            })
+          }
+        >
+          Saved Rooms
+        </button>
+        <button
+          href=""
+          onClick={() =>
+            handleOnClick({
+              title: "Sorry!",
+              body: "Currently, you do not have any contact request approved!",
+            })
+          }
+        >
+          Requested Contacts
+        </button>
       </div>
       <div className="container">
-      {
-        !message.show ?
-        <EditProfile /> :
-        <OtherPages message={message}  />
-      }
+        {
+          // show &&
+          message.title === "" ? (
+            <EditProfile />
+          ) : (
+            <OtherPages message={message} />
+          )
+        }
       </div>
     </div>
   );
--- src/components/profile/otherPages/OtherPages.js
+++ src/components/profile/otherPages/OtherPages.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import './OtherPages.css'
+
 function OtherPages(props) {
 
   return (
-    <div>
+    <div className="other-div">
       <h4><FontAwesomeIcon icon="exclamation-circle" size="lg" color="#f7a231" />{props.message.title}</h4>
       <p>{props.message.body}</p>
     </div>
